1fd0048553f9ed9665cc185583faf1ca8deaf251
johmcg <103613418+johmcg@users.noreply.github.com>
2023-03-31 16:42:24 -0400

    Fixed Incorrect Image Size

--- js/autoComplete.js
+++ js/autoComplete.js
@@ -22,7 +22,7 @@ function autoComplete(inputValue) {
 }
 
 function addItem(value) {
-    ulField.innerHTML = ulField.innerHTML + `<li class = "mt-3 mb-3">${value}</li>`;
+    ulField.innerHTML = ulField.innerHTML + `<li class = "p-1">${value}</li>`;
 }
 
 function selectItem({target}) {
